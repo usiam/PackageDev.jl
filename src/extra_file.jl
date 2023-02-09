eb9fc454ce9c0a77be84392baa34882232343148
Tahamid <usiamjobs@gmail.com>
2023-02-09 17:19:24 -0500

    Add `derivative_of_my_f` function to calculate derivatives

--- src/extra_file.jl
+++ src/extra_file.jl
@@ -1,1 +1,5 @@
 my_f(x, y) = 2x + 3y
+
+# must import the ForwardDiff package in the module (PackageDev in this case) to use this
+derivative_of_my_f(x, y) = ForwardDiff.derivative(x -> my_f(x, y), x)
+
