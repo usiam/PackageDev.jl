41cf737f4aa0d994ebf59417da7113cc65c9cb21
Tahamid <usiamjobs@gmail.com>
2023-02-08 20:32:01 -0500

    Add the `my_f` function in `extra_file.jl` `my_f` is a linear function for demonstration purposes `runtests.jl` includes testing for `my_f`

--- src/PackageDev.jl
+++ src/PackageDev.jl
@@ -1,5 +1,17 @@
 module PackageDev
 
-# Write your package code here.
+# Everything runs from the module which is the top level module with the same name as the package
+
+greet(name) = println("Hello $name!")
+
+# We don't want to put everything in one file so
+include("extra_file.jl")
+
+# export the function my_f from the `extra_file.jl` file to making it directly available for 
+# anyone who does `using`
+export my_f
+
+# use export mostly for others to use interactively.
+# Anything internal should not be exported as a rule of thumb.
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,19 @@
 using PackageDev
 using Test
 
+## ADD TESTS HERE ##
+
+# works if exported
+my_f(2, 1)
+
+# have to specify where the function lives if not exported
+PackageDev.my_f(2, 1)
+
 @testset "PackageDev.jl" begin
-    # Write your tests here.
+    # my_f(x, y) = 2x + 3y
+    @test my_f(2, 1) == 7
+    @test my_f(2, 3) == 13
+
 end
+
+# to test open the REPL and go to pkg using ] and then run `test <PackageName>`
