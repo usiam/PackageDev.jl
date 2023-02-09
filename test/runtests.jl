86ded180ec5cf45c77b7fab1b939561fe1ee446d
Tahamid <usiamjobs@gmail.com>
2023-02-08 20:53:01 -0500

    Fix test

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,7 +13,7 @@ PackageDev.my_f(2, 1)
     # my_f(x, y) = 2x + 3y
     @test my_f(2, 1) == 7
     @test my_f(2, 3) == 13
-    @test my_f(2, 3) == 11
+    @test my_f(1, 3) == 11
 end
 
 # to test open the REPL and go to pkg using ] and then run `test <PackageName>`
